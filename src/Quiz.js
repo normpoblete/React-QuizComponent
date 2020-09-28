bbec498ca5857a28f294707488a9707e03d20cdf
Norm Poblete <npoblete@sbmcorp.com>
2020-09-28 16:17:22 -0700

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,3 +1,13 @@
+import { render } from "enzyme"
 import React, { Component } from "react"
 
 let quizData = require('./quiz_data.json')
+
+class App extend Component {
+    render() {
+        return (
+            <div/>
+            <quizData/>
+        )
+    }
+}
