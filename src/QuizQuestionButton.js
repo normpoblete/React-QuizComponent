47484aa3dedff7eca97d11f457bc9100af962404
Norm Poblete <npoblete@sbmcorp.com>
2020-10-08 16:22:04 -0700

    Update QuizQuestionButton.js

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component {
+    return (
+        <li>
+            <button></button>
+        </li>
+    )
 
 }
 export default QuizQuestionButton
