d5dd241898b27e075c8cdd8dc79852dabdbac8a9
Norm Poblete <npoblete@sbmcorp.com>
2020-10-08 16:49:24 -0700

    Update QuizQuestionButton.js

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
 
 class QuizQuestionButton extends Component {
     render() {
         return (
-            <li>
-                <button>(this.props.button_text)</button>
-            </li>
+        <li>
+            <button>{this.props.button_text}</button>
+        </li>
         )
     {
 }
