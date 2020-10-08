498d0d060fb69b63e6ed4edf5a73124f7f6abd9e
Norm Poblete <npoblete@sbmcorp.com>
2020-10-08 13:50:19 -0700

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,7 +9,7 @@ class QuizQuestion extends Component {
                 </section>
                 <section>
                     <ul>
-                        <li>(this.props.quiz_question.answer_options[0]}</li>)
+                        <li>{this.props.quiz_question.answer_options[0]}</li>)
                     </ul>
                 </section>
             </main>
