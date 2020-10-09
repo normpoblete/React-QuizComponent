39c58ac8e231e2d01381e7b66242ae26b65d7b92
Norm Poblete <npoblete@sbmcorp.com>
2020-10-09 13:00:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import Quiz from './Quiz.js'
 import './App.css'
+
+import Quiz from './Quiz.js'
 
 class App extends Component {
   render() {
     return (
+      <div />
       <Quiz />
     )
   }
 }
-
-export default App
