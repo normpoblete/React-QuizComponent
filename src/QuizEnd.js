53469cafd493230d7eac3304c2798a4c711eeb60
Norm Poblete <npoblete@sbmcorp.com>
2020-10-08 17:43:32 -0700

    Update QuizEnd.js

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 
 class QuizEnd extends Component {
-
+    render() {
+        <div>
+            <p>Thanks for playing.</p>
+            <a href=''>Reset Quiz</a>
+        </div>
+    }
 }
 export default QuizEnd
